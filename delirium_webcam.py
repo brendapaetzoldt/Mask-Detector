fe7040738b5cb93e489a6940423371c6e9880489
Brenda Paetzoldt Silva <brendapaetzoldt@yahoo.com.br>
2021-04-04 15:25:29 -0300

    Máscaras usadas corretamente sendo identificadas

--- delirium_webcam.py
+++ delirium_webcam.py
@@ -16,7 +16,11 @@ while captura.isOpened():
         objetosDetectados = detector(frame, 1)
         for o in objetosDetectados:
             e, t, d, f = (int(o.left()), int(o.top()), int(o.right()), int(o.bottom()))
-            cv2.rectangle(frame, (e, t), (d, f), (0, 0, 255), 2)
+            cv2.rectangle(frame, (e, t), (d, f), (255, 0, 0), 2)
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, "Mascara usada corretamente", (0, 130), font, 1, (300, 250), 2, cv2.LINE_AA)
+
         cv2.imshow("Preditor de Objetos", frame)
 
         if cv2.waitKey(1) & 0xFF == 27:
